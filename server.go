73314c00d1789f2f23c451f87c339b06902eda0a
white105 <nickwhite1423@gmail.com>
2019-12-31 21:39:02 -0500

    add endpoint for code retrieval

--- server.go
+++ server.go
@@ -13,6 +13,7 @@ func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
     myRouter.HandleFunc("/login", login).Methods("POST")
     myRouter.HandleFunc("/register", register).Methods("POST")
+    myRouter.HandleFunc("/code", register).Methods("POST")
     log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(myRouter)))
 }
 
@@ -30,6 +31,13 @@ func register(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
+  fmt.Println("you just hit this endpoint yo")
+  reqBody, _ := ioutil.ReadAll(r.Body)
+  fmt.Println("request body", string(reqBody))
+  fmt.Fprintf(w, "%+v", string(reqBody))
+}
+
+func runCode(w http.ResponseWriter, r *http.Request) {
   fmt.Println("you just hit this endpoint yo")
   reqBody, _ := ioutil.ReadAll(r.Body)
   fmt.Println("request body", string(reqBody))
